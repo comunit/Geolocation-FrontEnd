cb5bbc884ffc6bace9220e10c9c359a1cbf0befe
Imran Rafique <comunit76@gmail.com>
2018-01-09 21:58:15 +0000

    fixing some things

--- getPosition.js
+++ getPosition.js
@@ -1,9 +1,13 @@
+// Get vars
 var btn = document.getElementById('btn');
 var map = document.getElementById('map');
 var container = document.getElementById('container');
 var userName = document.getElementById('userName');
+
+//Fire getMap when btn is clicked
 btn.addEventListener('click', getMap);
 
+// gep map function to get username and initialize map
 function getMap() {
   container.style.display = "none";
   map.style.display = "block";
@@ -16,6 +20,7 @@ function getMap() {
   });
 }
 
+//Map initialize here
 function initMap(userName) {
   var map = new google.maps.Map(document.getElementById('map'), {
     zoom: 10,
@@ -25,50 +30,79 @@ function initMap(userName) {
     }
   });
 
-  var userIds = [];
+  // set vars to store users information and keep track of markers
   var markers = [];
-  
+  var users = [];
+  var userIds = [];
 
-  // setInterval(function(){ 
-  if (navigator.geolocation) {
-    navigator.geolocation.getCurrentPosition(showPosition)
-  } else {
-    alert('Geolocation is not supported by this browser');
-  }
+  // Get location of user
+  setInterval(function () {
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(showPosition)
+    } else {
+      alert('Geolocation is not supported by this browser');
+    }
+  }, 10000);
 
 
+  //send position to server  every 5 seconds
   function showPosition(position) {
-    //Send Position to server every second
     socket.emit('location', {
       lat: position.coords.latitude,
       lng: position.coords.longitude,
       user: userName,
       id: socket.id
     });
-    
 
+
+    //listen for events
     socket.on('location', function (data) {
-
       var data = data.loc;
+      // loop through data coming from server
       for (let i = 0; i < data.length; i++) {
         const element = data[i];
-        var checkUserId = userIds.indexOf(element.id);
+        //create a object of users and thier location
+        getuser = {
+          lat: element.lat,
+          lng: element.lng,
+          user: element.user,
+          id: element.id
+        }
+        //if user already exists in users update thier locaation else add to user
+        let obj = users.find(o => o.id === element.id);
+        if (obj == undefined) {
+          users.push(getuser);
+        } else {
+          let obj = users.find((o, i) => {
+            //Find index of user using user method.    
+            objIndex = users.findIndex((obj => obj.id == element.id));
+            //Update object's lat and lng property.
+            users[objIndex].lat = element.lat;
+            users[objIndex].lng = element.lng;
+            users[objIndex].user = element.user;
+          });
+        }
 
+        var checkUserId = userIds.indexOf(element.id);
         if (checkUserId == -1) {
           userIds.push(element.id)
-          var pos = {
-            lat: element.lat,
-            lng: element.lng,
+
+          for (let i = 0; i < users.length; i++) {
+            const mark = users[i];
+            test = new google.maps.InfoWindow;
+            test.setContent(mark.user);
+            test.open(map);
+            test.setPosition({lat: mark.lat, lng: mark.lng});
+            test.set("id", mark.id);
+            markers.push(test);
           }
-          test = new google.maps.InfoWindow;
-          test.setContent(element.user);
-          test.open(map);
-          test.setPosition(pos);
-          test.set("id", element.id);
-          markers.push(test);
+          console.log(users);
+          console.log(markers);
         }
+          
       }
     });
+
 
     // Handle Disconnted User
     socket.on('disconnectId', function (data) {
@@ -77,6 +111,12 @@ function initMap(userName) {
         if (data.disconnetId == element.id) {
           markers[i].setMap(null);
         }
+      }
+
+      for (let i = 0; i < users.length; i++) {
+        const user = users[i];
+        objIndex = users.findIndex((obj => obj.id == data.disconnetId));
+        users.splice(objIndex, 1);
       }
     });
   }
